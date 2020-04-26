cd449d70a157892a6fe6eb5517e45c8a55b10794
TanishqSingla <tanishqsingla00@gmail.com>
2020-04-26 19:11:17 +0530

    saving state of input
    Some design improvements

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [inputText, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+  };
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input type="text" value={inputText} onChange={handleChange} />
         <button>
           <span>Add</span>
         </button>
